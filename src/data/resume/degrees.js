7ef6539231b3796ef6693069147464dba3a9a87e
Konrad Muczynski <muczynskik@gmail.com>
2024-11-26 18:55:30 +0100

    Added  courses & uni

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,16 +1,11 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'University of Surrey',
+    degree: 'BSc in Computer Science',
+    link: 'https://surrey.ac.uk',
+    year: '2016 - 2019',
   },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
-  },
+
 ];
 
 export default degrees;
--- src/data/resume/skills.js
+++ src/data/resume/skills.js
@@ -1,13 +1,8 @@
 const skills = [
   {
     title: 'Javascript',
-    competency: 4,
+    competency: 2,
     category: ['Web Development', 'Languages', 'Javascript'],
-  },
-  {
-    title: 'Node.JS',
-    competency: 5,
-    category: ['Web Development', 'Javascript'],
   },
   {
     title: 'React',
@@ -16,71 +11,41 @@ const skills = [
   },
   {
     title: 'Next.JS',
-    competency: 3,
-    category: ['Web Development', 'Javascript'],
-  },
-  {
-    title: 'Bash',
     competency: 2,
-    category: ['Tools', 'Languages'],
+    category: ['Web Development', 'Javascript'],
   },
   {
     title: 'Amazon Web Services',
-    competency: 4,
-    category: ['Web Development', 'Tools'],
-  },
-  {
-    title: 'Heroku',
-    competency: 2,
-    category: ['Web Development', 'Tools'],
-  },
-  {
-    title: 'MongoDB',
     competency: 3,
-    category: ['Web Development', 'Databases'],
+    category: ['Web Development', 'Tools'],
   },
   {
-    title: 'ElasticSearch',
-    competency: 2,
-    category: ['Web Development', 'Databases'],
-  },
-  {
-    title: 'PostgreSQL/SQLite3/SQL/Redshift',
-    competency: 4,
+    title: 'PostgreSQL/SQL/Redshift',
+    competency: 5,
     category: ['Web Development', 'Databases', 'Languages'],
   },
   {
-    title: 'Redis',
-    competency: 3,
-    category: ['Web Development', 'Databases'],
-  },
-  {
     title: 'Data Mining',
-    competency: 3,
+    competency: 4,
     category: ['ML Engineering'],
   },
   {
-    title: 'Express.JS',
-    competency: 2,
-    category: ['Web Development', 'Javascript'],
-  },
-  {
-    title: 'D3',
-    competency: 2,
-    category: ['Web Development', 'Javascript'],
-  },
-  {
     title: 'Flask',
-    competency: 3,
+    competency: 2,
+    category: ['Web Development', 'Python'],
+  },
+  {
+    title: 'Django',
+    competency: 2,
     category: ['Web Development', 'Python'],
   },
   {
     title: 'FastAPI',
-    competency: 3,
+    competency: 2,
     category: ['Web Development', 'Python'],
   },
   {
-    title: 'Git/Mercurial',
+    title: 'Git',
     competency: 3,
     category: ['Tools'],
   },
@@ -90,9 +55,9 @@ const skills = [
     category: ['Tools', 'Data Engineering'],
   },
   {
-    title: 'Google Cloud Compute',
-    competency: 2,
-    category: ['Tools', 'Web Development'],
+    title: 'Google Analytics',
+    competency: 5,
+    category: ['Tools'],
   },
   {
     title: 'AWS',
@@ -101,7 +66,7 @@ const skills = [
   },
   {
     title: 'Docker',
-    competency: 3,
+    competency: 2,
     category: ['Tools', 'Data Engineering'],
   },
   {
@@ -111,13 +76,8 @@ const skills = [
   },
   {
     title: 'Numpy',
-    competency: 3,
+    competency: 4,
     category: ['Data Science', 'Data Engineering', 'Python', 'ML Engineering'],
-  },
-  {
-    title: 'Numba',
-    competency: 2,
-    category: ['Data Science', 'Data Engineering', 'Python'],
   },
   {
     title: 'Tensorflow + Keras',
@@ -126,18 +86,13 @@ const skills = [
   },
   {
     title: 'PyTorch',
-    competency: 3,
+    competency: 2,
     category: ['ML Engineering', 'Python'],
   },
   {
     title: 'Jupyter',
-    competency: 3,
+    competency: 5,
     category: ['Data Science', 'Python'],
-  },
-  {
-    title: 'Typescript',
-    competency: 3,
-    category: ['Web Development', 'Languages', 'Javascript'],
   },
   {
     title: 'HTML + SASS/SCSS/CSS',
@@ -150,21 +105,6 @@ const skills = [
     category: ['Languages', 'Python', 'ML Engineering'],
   },
   {
-    title: 'Ruby',
-    competency: 2,
-    category: ['Languages'],
-  },
-  {
-    title: 'Ruby on Rails',
-    competency: 3,
-    category: ['Web Development', 'Languages'],
-  },
-  {
-    title: 'C++',
-    competency: 1,
-    category: ['Languages'],
-  },
-  {
     title: 'Julia',
     competency: 2,
     category: ['Languages'],
@@ -175,13 +115,8 @@ const skills = [
     category: ['Languages'],
   },
   {
-    title: 'R',
-    competency: 2,
-    category: ['Languages'],
-  },
-  {
     title: 'Data Visualization',
-    competency: 3,
+    competency: 5,
     category: ['Data Science', 'Javascript'],
   },
   {
@@ -192,6 +127,11 @@ const skills = [
   {
     title: 'Pandas',
     competency: 5,
+    category: ['Data Engineering', 'ML Engineering', 'Python'],
+  },
+  {
+    title: 'Polars',
+    competency: 2,
     category: ['Data Engineering', 'ML Engineering', 'Python'],
   },
   {
@@ -208,21 +148,6 @@ const skills = [
     title: 'Spark',
     competency: 2,
     category: ['Data Engineering', 'ML Engineering'],
-  },
-  {
-    title: 'Dagster',
-    competency: 2,
-    category: ['Data Engineering', 'Python', 'ML Engineering'],
-  },
-  {
-    title: 'Mypy',
-    competency: 3,
-    category: ['Python'],
-  },
-  {
-    title: 'Pylint',
-    competency: 4,
-    category: ['Data Engineering', 'Python'],
   },
 ].map((skill) => ({ ...skill, category: skill.category.sort() }));
 
