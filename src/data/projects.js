55185222f1fbdb90170fc4a47c4540915b423d77
Konrad Muczynski <muczynskik@gmail.com>
2024-11-27 19:49:23 +0100

    grammatical and punctual errors.

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ const data = [
     image: '/images/projects/harvest.jpg',
     date: '2024-09-20',
     desc:
-      'I analyze data from the Institute of Meteorology and Water Management(IMGW) '
+      'I analyze data from the Institute of Meteorology and Water Management (IMGW) '
       + 'to identify areas in Poland with the strongest wind potential. Using AI models,'
       + 'I predict optimal locations for wind energy farms,'
       + 'helping to maximize efficiency and support sustainable energy development.',
